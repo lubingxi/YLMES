7506fb21006fdb71178a234e2cabc139b91f9ef8
1436544780 <1436544780@qq.com>
2018-11-07 18:56:10 +0800

    2

---
+++ YLMES/Controllers/SystemSettingsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YLMES.Controllers
+{
+    public class SystemSettingsController : Controller
+    {
+        // GET: SystemSettings
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region 权限设置
+
+        public ActionResult Permissions()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
